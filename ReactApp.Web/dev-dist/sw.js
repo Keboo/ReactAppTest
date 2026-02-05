1fa58e33bec49ff454bc6cd0de8c77edd4553b91
Kevin Bost <kitokeboo@gmail.com>
2026-02-05 12:52:33 -0800

    Refreshes frontend assets and dependencies
    Updates the Workbox service worker and its source map, reflecting a fresh build of frontend assets.
    
    Refreshes the package-lock to align with current dependency metadata, including updates to peer dependency flags.

--- ReactApp.Web/dev-dist/sw.js
+++ ReactApp.Web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7las3hf3kuo"
+    "revision": "0.csj60fa4rfg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
